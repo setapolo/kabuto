5a9c79ff7f32b607c92b6f64c99203c5b58424dc
moka <oka@tij.ne.jp>
2018-10-27 10:12:40 +0900

    #002,003 init

--- readme.js
+++ readme.js
@@ -4,4 +4,17 @@
 (function(s){
   console.log(s);
 })("abcdefg");
-//
+
+//#002 a is Array
+(function(s){
+  var a = s.split("");		
+  console.log(a);
+})("abcdefg");
+
+//#003 array to string
+(function(a){
+  var s = a.join("")		
+  console.log(s);
+})(["a","b","c","d"]);
+
+
