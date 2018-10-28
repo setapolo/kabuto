6bbaf0715a4d182d627bb0bc73d13cc1094e3443
岡 実 <okamino@gmail.com>
2018-10-28 21:03:51 +0900

    tuple take2

--- tuple.js
+++ tuple.js
@@ -17,3 +17,26 @@
 	("row5")
 	()
 );
+// tuple take2
+//var html = 
+
+(function (s) {
+	console.log("tuple",s);
+})(
+	(function(s){
+		var a=[s];		
+		var f=function(s){
+			a = (!s)? a : a.concat(s);  
+			return (!s)?(a):f;
+			}
+		return f;
+	})
+	("row1")
+	("row2")
+	("row3")
+	("row4")
+	("row5")
+	().map((function(c,i,a){
+	return ["<tr>",c,"</tr>"].join();
+}))
+);
