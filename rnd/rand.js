69c363a26bbb59e628c2a8f5949888648c8d45be
岡 実 <okamino@gmail.com>
2018-11-03 06:27:55 +0900

    rand reel1 memo

--- rnd/rand.js
+++ rnd/rand.js
@@ -9,3 +9,19 @@
 	})
 	(12345)
 );
+
+//middle-square method  very rough
+//reel 0
+(function(s){
+	console.log("reel0",s)
+})(
+	(function(i){
+		f =function(j){
+			var l = (i).toString().length;
+			return (i*i).toString().substring(2,l+2);
+
+		}
+		return f;
+	})
+	(12345)
+);
