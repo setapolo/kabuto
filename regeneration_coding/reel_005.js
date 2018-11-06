b7dc1746c756bf095989462ca32ff68d1c8b5e4f
岡 実 <okamino@gmail.com>
2018-11-07 06:17:49 +0900

    memo

---
+++ regeneration_coding/reel_005.js
@@ -0,0 +1,19 @@
+var jslinks = (function(){
+/*
+https://code.jquery.com/jquery-1.12.4.js
+https://code.jquery.com/ui/1.12.1/jquery-ui.js
+*/
+}).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "");
+console.log(jslinks.split("\n").map(function(c,i,a){return '<script src="URL"></script>'.replace(/URL/ig,c);}));
+
+
+(function(s){
+  console.log(s);
+})
+ ( (function(HEAD,TAIL){
+   // arguments(HEAD)
+   // arguments(TAIL)
+    return [HEAD,"(",TAIL,")"]
+  })("0","1")
+)
+//["<",HEAD,">"]
