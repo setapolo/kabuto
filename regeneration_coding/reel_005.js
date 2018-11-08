ccb9a77b396e71c8466703f04db7b7bf64eb1c31
岡 実 <okamino@gmail.com>
2018-11-08 09:01:29 +0900

    reduce&template#1

--- regeneration_coding/reel_005.js
+++ regeneration_coding/reel_005.js
@@ -15,5 +15,46 @@ console.log(jslinks.split("\n").map(function(c,i,a){return '<script src="URL"></
    // arguments(TAIL)
     return [HEAD,"(",TAIL,")"]
   })("0","1")
-)
+);
 //["<",HEAD,">"]
+
+
+(function(s){
+  console.log(s);
+})(
+  [].reduce.call(
+    ["a",["b","c","d"],["b","c","d"],["b","c","d"]]
+    ,function(m,c,i,a){
+      var arg = arguments;
+      var a=[0,"(",1,")"].map(
+        function(c,i,a){
+          return isNaN(c)?c:arg[c];  
+        })
+      return a.join("");
+       }
+    )
+);
+/*
+//    var a=[arguments[0],"(",arguments[1],")"];
+//    return arguments[0]+"("+arguments[1]+")";
+//    return m+"("+c+")";
+arguments[0]
+double arguments
+f=function(a){
+  arguments[a[0]];
+}
+f(arguments)
+[HEAD,'(',MID,')']
+["<",HEAD,">"]これでフォームがきまる
+
+実際の情報は流し込み
+
+()printf abc efg
+(cfunction)
+
+
+
+[0,'(',1,')']これが実態
+クロージャ多様
+arguments[HEAD]
+*/
