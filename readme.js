2718ffe7babd0dfd5124793f20a86ba62e1b5b95
岡 実 <okamino@gmail.com>
2018-11-09 22:45:58 +0900

    tidy

--- readme.js
+++ readme.js
@@ -6,36 +6,51 @@
 })("abcdefg");
 
 //#001_002 a is Array
-(function(s){
-  var a = s.split("");		
-  console.log(a);
-})("abcdefg");
+(function(a){
+	console.log(a);
+})(
+	(function(s){
+		return s.split("");		
+	})("abcdefg")
+);
 
 //#001_002 j is integer
-(function(s){
-  var j = parseInt(s);		
-  console.log(j);
-})("12345");
+(function(j){
+	console.log(j);
+})(
+	(function(s){
+	  return parseInt(s);		
+	})("12345")
+);
 
 //#003 array to string
-(function(a){
-  var s = a.join("");		
-  console.log(s);
-})(["a","b","c","d"]);
+(function(s){
+	console.log(s);
+})(
+	(function(a){
+	  return a.join("");		
+	})(["a","b","c","d"])
+);
 
 //#003_002 test data test trail
-(function(a){
-  var s = a.join("");		
-  console.log(s);
-//})(["a","b","c","d"]);
-})(["a","b","c","d"]);
+(function(s){
+	console.log(s);
+})(
+	(function(a){
+	  return a.join("");		
+	//})(["a","b","c","d"]);
+	})(["a","b","c","d"])
+);
 
 //#003_003 null trail
-(function(a){
-  var s = (a) ? a.join(""):null;
-  console.log(s);
-//})(["a","b","c","d"]);
-})(null);
+(function(s){
+	console.log(s);
+})(
+	(function(a){
+  		return (a) ? a.join(""):null;
+	//})(["a","b","c","d"]);
+	})(null)
+);
 
 //#003_004 undefined trail
 (function(a){
