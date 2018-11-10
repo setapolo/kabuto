4f73a64bf2d2096d694ed9a5774978b6af7246f8
岡 実 <okamino@gmail.com>
2018-11-10 23:36:57 +0900

    refactor

--- readme.js
+++ readme.js
@@ -2,96 +2,119 @@
 
 //#001_001 s represents String
 (function(s){
-  console.log(s);
-})("abcdefg");
+	console.log("",s);
+})("abcdefg")
+;
 
 //#001_002 a is Array
 (function(a){
-	console.log(a);
+	console.log("",a);
 })(
 	(function(s){
 		return s.split("");		
 	})("abcdefg")
-);
+)
+;
 
 //#001_002 j is integer
 (function(j){
-	console.log(j);
+	console.log("",j);
 })(
 	(function(s){
 	  return parseInt(s);		
 	})("12345")
-);
+)
+;
 
 //#003 array to string
 (function(s){
-	console.log(s);
+	console.log("",s);
 })(
 	(function(a){
 	  return a.join("");		
 	})(["a","b","c","d"])
-);
+)
+;
 
 //#003_002 test data test trail
 (function(s){
-	console.log(s);
+	console.log("",s);
 })(
 	(function(a){
 	  return a.join("");		
 	//})(["a","b","c","d"]);
 	})(["a","b","c","d"])
-);
+)
+;
 
 //#003_003 null trail
 (function(s){
-	console.log(s);
+	console.log("",s);
 })(
 	(function(a){
   		return (a) ? a.join(""):null;
 	//})(["a","b","c","d"]);
 	})(null)
-);
+)
+;
 
 //#003_004 undefined trail
-(function(a){
-  var s = (a) ? a.join(""):null;
-  console.log(s);
-//})(["a","b","c","d"]);
-})(undefined);
+(function(s){
+	console.log("003_004",s);
+})(
+	(function(a){
+  		return (a) ? a.join(""):null;
+	//})(["a","b","c","d"]);
+	})(undefined)
+)
+;
 
 //#003_005 Nan trail, reduplication
-(function(j){
-  var s =  (!isNaN(parseInt(j))) ? parseInt(j):null;		
-  console.log(s);
-//})("1234");
-//})(null);
-//})(1234);
-})("one");
+(function(s){
+	console.log("003_005",s);
+})(
+	(function(s){
+	  return (!isNaN(parseInt(s))) ? parseInt(s):null;		
+	//})("1234");
+	//})(null);
+	//})(1234);
+	})("one")
+)
+;
 
 
 
 //#003_006 Nan trail 
-(function(j){
- j= !isNaN(j)	? j:null;
- console.log(j);
+(function(s){
+	console.log("003_006",s);
 })(
-parseInt('one')
-);
+	(function(j){
+		return !isNaN(j)?j:null;
+	})(
+		parseInt('one')
+	)
+)
+;
 
 //#004_001 j plus i
-(function(j){
-	return function(){
-		j++;
-		console.log(j);
-		return this;
-	};
-})(1)();
+(function(s){
+//	console.log("004_001",s);
+})(
+	(function(j){
+		return function(){
+			j++;
+			console.log("004_001",j);
+			return this;
+		};
+	})(1)()
+)
+;
 
 //#014_001 function's functions
 (function(j){
-	console.log("start"+j);
+	console.log("014_001start"+j);
 	f= (function(){
-		console.log("a"+j);
+		console.log("014_001a"+j);
 		j++;
 		return f
 	});
@@ -100,13 +123,13 @@ parseInt('one')
 
 //#014_002 function's functions
 (function(j){
-	console.log("start"+j);
+	console.log("014_002start"+j);
 	f= (function(i){
-		console.log("odd"+j);
+		console.log("014_002odd"+j);
 		console.log("i"+i);
 		j++;
 		return function(ii){
-			console.log("even"+j);			
+			console.log("014_002even"+j);			
 			console.log("ii"+ii);
 			j++;
 			return f;	
@@ -114,6 +137,7 @@ parseInt('one')
 	});
 	return f;
 })(1)(2)(3)(4)(5)(6)()()()()()()()()();
+
 
 //#015_001 hdoc
 console.log(
