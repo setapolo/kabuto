a12ac9b976693cc07bd6fec5f2e4abcfd7724742
moka <oka@tij.ne.jp>
2018-11-10 10:51:46 +0900

    with heredoc

--- splitreduce_mapjoin/reel_001.js
+++ splitreduce_mapjoin/reel_001.js
@@ -4,10 +4,45 @@
 	(function(s){
 		return s.split("").map(
 			function(c,i,a){
-				return [i,":","[",c,"]"].join(" ")
+				return ["[",c,"]"].join(" ")
 			}
 		).reduce(function(m,c,i,a){
 			return [m,c].join(" ");
 		})
-	})("123456|78910")
+	})("123456")
+);
+
+
+(function(s){
+	console.log("sprmaj",s);
+})(
+	(function(s){
+		return s.split("").reduce(
+			function(m,c,i,a){
+				return [m,"(",c,")"].join(" ")
+			}
+		)
+	})("123456")
+);
+
+
+(function(s){
+	console.log("sprmaj",s);
+})(
+	(function(s){
+		return s.split("\n").map(
+			function(c,i,a){
+				return ["(",c,")"].join("\n")
+			}
+		)
+	})(
+(function(){
+/*
+12345
+67890
+abcde
+ABCDE
+*/	
+}).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "")
+	)
 );
