9fff3042bf82164d924762985d88dee5e7982060
moka <oka@tij.ne.jp>
2018-11-10 18:08:58 +0900

    cascade

--- functions/reel_001.js
+++ functions/reel_001.js
@@ -1,12 +1,12 @@
 //base
 (function(s){
-	console.log("base",s)
+	console.log("base1",s)
 })(
 );
 
 //base
 (function(s){
-	console.log("base",s)
+	console.log("base2",s)
 })(
 	(function(a){
 		(function(i){
@@ -40,3 +40,39 @@
 	})([])	
 );
 
+//base
+(function(s){
+	console.log("base2",s)
+})(
+	(function(aa){
+		(function(i){
+			aa=["head"];
+			var f= (function(i){
+				var a = [arguments];
+				return (function(ii){
+					a = a.concat("ii");
+					return (function(iii){
+						a = a.concat("iii");
+						return (function(iiii){
+							a = a.concat("iiii");
+							aa.push(a);
+							return f;
+						})						
+					})
+				})	
+			});
+			return f;
+		})(function(){return 1})
+		(1)(2)(3)(4)
+		(5)(6)(7)(8)
+		()()()()
+		()()()()
+		()()()()
+		()()()()
+		()()()()
+		()()()()
+		()()()()
+		()()()()
+		return aa;
+	})([])	
+);
