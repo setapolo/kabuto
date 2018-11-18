9435837c2c0446ca4c6fefeaca10290a899c5a5f
岡 実 <okamino@gmail.com>
2018-11-18 15:10:20 +0900

    join

--- base/reel_003.js
+++ base/reel_003.js
@@ -42,19 +42,75 @@ console.log("check#1",s);
 
 var CR=["\n"];
 var HEADTAIL=[1,-1];
-var s="12\n34\n56\n78\n90";
+var s=`
+12
+34
+56
+78
+90
+`;
 var a=[
 [''.split,CR],
 [[].slice,HEADTAIL],
-[''.join,CR]
-]
+[[].join,CR]
+];
+a.map(function(c,i,a){
+  s = c[0].apply(s,c[1]);
+  return c;
+})
 console.log("check#2",s);
+
+
+var CR=["\n"];
+var HEADTAIL=[1,-1];
+var s=`
+12
+34
+56
+78
+90
+`;
+var a=[
+[''.split,CR],
+[[].slice,HEADTAIL],
+[[].join,CR]
+].map(function(c,i,a){
+  s = c[0].apply(s,c[1]);
+  return c;
+})
+console.log("check#3",s);
+
+
+(function(s){
+  console.log("check#4",s);
+})(
+(function(a){
+  var CR=["\n"];
+  var HEADTAIL=[1,-1];
+  return [[
+  `
+12
+34
+56
+78
+90
+  `],
+  [''.split,CR],
+  [[].slice,HEADTAIL],
+  [[].join,CR]
+  ].reduce(function(m,c,i,a){
+    return c[0].apply(m,c[1]);
+  })
+}([]))
+);
+
 
 
 (function(){
   var CR=["\n"];
   var HEADTAIL=[1,-1];
-  return (function(){
+  return 
+  (function(){
     var f =(function(){
       return f;
     });
@@ -65,3 +121,6 @@ console.log("check#2",s);
   (''.join,CR)
   ;
 })();
+
+
+
