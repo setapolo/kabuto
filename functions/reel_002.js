360670faff6126cb7d96d2536887367b7860f1aa
岡 実 <okamino@gmail.com>
2018-11-25 19:54:42 +0900

    arc

--- functions/reel_002.js
+++ functions/reel_002.js
@@ -2,3 +2,14 @@
 	console.log(i);
 	return i;
 })
+
+console.log(
+" ".repeat(360).split("").map(function(c,i,a){
+	console.log("i:",i);
+	console.log("cos:",Math.floor(Math.cos(i*Math.PI/180)*100+100));
+	console.log("sin:",Math.floor(Math.sin(i*Math.PI/180)*100+100));
+	var x = Math.floor(Math.cos(i*Math.PI/180)*100+100);
+	var y = Math.floor(Math.sin(i*Math.PI/180)*100+100);
+	return [x,y];
+})
+);
