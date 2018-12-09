7aae9da26a3566f3edaaa68b6e863ef31d118ab8
moka <oka@tij.ne.jp>
2018-12-09 13:25:31 +0900

    1st approach of "Trush can" implementation. https://en.wikipedia.org/wiki/Garbage_can_model

--- array/is.js
+++ array/is.js
@@ -4,7 +4,7 @@ var is = (function(rr){
 	return function(s){
 		rr['alphabet'] = /^[a-zA-Z]+$/;
 		return (s)?(function(c){
-			return rr[s].test(c);
+			return (rr[s])?rr[s].test(c):true;
 		}):rr;
 	}
 })({})
@@ -13,5 +13,16 @@ console.log(["123456","english"].filter(is("alphabet")));
 var is = (function(rr){
 	rr['number'] = /^[0-9]+$/;
 	return is;
-})(is())
+})(is());
 console.log(["123456","english"].filter(is("number")));
+console.log(["123456","english"].filter(is("others")));
+
+var only = (function(rr){
+	rr['number'] = /^[0-9]+/;
+	return function(s){
+		return (s)?(function(c){
+			return (rr[s])?rr[s].exec(c).toString():true;
+		}):rr;
+	}
+})(is());
+console.log(only("number")("123456english"));
