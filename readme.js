083521470db86fe411ab001aa854dd3923b668e8
moka <oka@tij.ne.jp>
2019-02-03 21:11:42 +0900

    100

--- readme.js
+++ readme.js
@@ -75,70 +75,65 @@
     (1)(1)(1)(1)()(1)()(1)();
 
 //#014_006 arrrray / Godzilla 
-(function(a){
+var a = (function(a){
 	var j=0;
-	console.log("014_006 top",a);
 	var f = (function(i){
-		console.log("014_006 head",a);
 		var ff = function(ii){
-			return (ii)?tt(ii):f//tail as false case;
+			//tail as null case;
+			return (ii)?tt(ii):f
 		}
 		var tt = function(iii){
 			a[j]=j+iii;
 			j++;
-			console.log("014_006 body",a);
 			return ff;
 		}
-		return tt(i);
+		return (i)?tt(i):a;
 	});
 	return f;
-})([])
+})([])//Grrrrrrrrrrr
     (1)(1)(1)()
     (1)(1)()
  (1)(1)(1)(1)()
     (1)(1)(1)()
     (1)(1)()
-    (1)(1)()(1)(1)(1);
-
+    (1)(1)()(1)(1)(1)()();
+console.log("#014_006 arrrray / Godzilla",a)
 
 //#014_007 arrrray / 2d array 
-(function(a,j){
-	j=j || -1;
-	console.log("014_007 top",a);
+var a = (function(a,j){
+	j=j || -1;//stopgaps.....
 	var f = (function(i,jj){
 		jj=jj || 0;
 		j=j+1;
 		a[j]=[]
-		console.log("014_007 head",a);
 		var ff = function(ii){
-			return (ii)?tt(ii):f//tail as false case;
+			return (ii)?tt(ii):f//tail as null case;
 		}
 		var tt = function(iii){
 			a[j][jj]=jj+iii;
 			jj++;
-			console.log("014_007 body",a);
 			return ff;
 		}
-		return tt(i);
+		return (i)?tt(i):a;//flush as null case
 	});
 	return f;
 })([])
     (1)(1)(1)(1)()
     (1)(1)(1)(1)()
     (1)(1)(1)(1)()
-    (1)(1)(1)(1)()
-;
+    (1)(1)(1)(1)()();
+console.log("#014_007 arrrray / 2d array","\n",a)
 
 //#014_001_02 closure cascading meets method cascading ***Face hugger#2***
 var pp=function(j){
-	console.log("014_001_02 head",j);
+	console.log("014_001_02 head",j);//closure cascading
 	var f = (function(i){
 		console.log("014_001_02 body",i);
 		return f;
 	});
 	return f;
 };
-pp.
+pp.//method cascading
 call(this,1).
 	call(this,2).
 		call(this,3).
@@ -159,7 +154,12 @@ call(this,1).
 	call(this,2).
 		call(this,3).
 			call(this,4).
-				call(this,5);
+				call(this,5).
+					call(this,6).
+						call(this,7).
+							call(this,8).
+								call(this,9).
+									call(this,10);
 
 //#014_001_04 closure cascading ***Face hugger#3***
 [1,2,3,4,5].reduce(function (p,c,i,a){
@@ -204,7 +204,7 @@ reduce(function (p,c,i,a){
 })
 );
 
-//cc into mc
+//closure cascading meets method cascading
 console.log("014_001_06",
 	(function(rr){
 		var f = function(s){
@@ -217,8 +217,9 @@ console.log("014_001_06",
 		}
 		return f;	
 	})
-	({		split:function(s){return this.split(s); },
-			slice:function(s){return this.slice(s); }
+	({
+			split:function(){return "".split.apply(this,arguments); },
+			slice:function(){return [].slice.apply(this,arguments); }
 	})
 	(
 	`
@@ -227,6 +228,35 @@ In the forests of the night,
 What immortal hand or eye
 Could frame thy fearful symmetry?
 	`
-	)("split",[/\n/])("slice",[1,-1])
-)
+	)("split",["\n"])("slice",[1,-1])
+);
 
+//closure cascading meets method cascading
+console.log("014_001_07",
+	(function(rr){
+		var f = function(s){
+			var ff = function(cmd,arg){
+				s = rr[cmd].apply(s,arg);	
+				console.log("014_001_07",s);
+				return ff;	
+			}
+		return ff;
+		}
+		return f;	
+	})
+	({
+			split:function(){return "".split.apply(this,arguments); },
+			slice:function(){return [].slice.apply(this,arguments); },
+			splitTrim(r){
+				return "".split.apply(this,[r]).slice(1,-1);
+			}	
+	})
+	(
+	`
+Tyger! Tyger! burning bright
+In the forests of the night,
+What immortal hand or eye
+Could frame thy fearful symmetry?
+	`
+	)("splitTrim",["\n"])
+);
