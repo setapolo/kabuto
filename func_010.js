a78ecc7f026778dbf902e8e69a620618e796c853
岡 実 <okamino@gmail.com>
2020-01-01 17:49:17 +0900

    heredocument

---
+++ func_010.js
@@ -0,0 +1,87 @@
+(function(st){
+    var rr={ff:[],a:[],i:0};var back;var fs=[];var ret=1;
+    var set=function(c){console.log(rr.i++,":set",arguments);
+        (typeof c == "function")?
+            (rr[back]=c):0;
+        (typeof c == "number")?
+            (function(){
+                "_"['repeat'](c)['split']('')['map'](function(){
+                    rr.a.push(rr.a[rr.a.length-1])                                
+                });
+            }()):(
+        (typeof c == "string")?
+            (function(){
+                rr[c]=0;
+                back=c;
+            }()):
+        (c)?
+            (rr.a.push(c)):0);
+        return (c)?set:map;
+    };
+    var map=function(c){console.log(rr.i++,":map",arguments);
+        (typeof c == "function")?
+            (c.apply(rr,[rr])):0;
+        (typeof c == "string")?
+            ((rr[c])?(function(){fs.push(rr[c])}()):0):0;
+        (typeof c == "number")?
+            (function(){
+                "_"['repeat'](c)['split']('')['map'](function(cc,ii,aa){
+                    fs.map(function(ccc,iii,aaa){ret = ccc.apply(rr,[ret,ii])});
+                });
+                fs=[];
+            })():0;
+        (typeof c == "object")?
+            (c['map'])?
+            (function(){
+                c['map'](function(cc,ii,aa){
+                    fs.map(function(ccc,iii,aaa){
+                        ret = ccc.apply(rr,[ret,cc])
+                    });
+                });
+            }()):((c['source'])?(
+                function(){
+                    ret = rr[c['source']].apply(rr,[ret]);
+                    var cc = map(ret);
+                }()
+            ):0):0;
+
+        return (c)?map:set;
+    };
+    return set;
+})({})
+    ("x2")(function(c,rr){return c*2;})
+    ("x4")(function(c,rr){return c*4;})
+    ("ss")(function(){
+      return (function(){
+    /*
+     <tr id="word1">  
+        <td>word2</td>
+        <td width = "20%">
+          <div class="float-right">
+            <input type="button" onClick="rec_start('word1','word2');" value="rec"　>
+          </div>
+        </td>
+      </tr> 
+    */
+      }).toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "")
+    })
+    ("splitjoin")(function(c,rr){console.log("splitjoin::",c)
+        return (c['join'])?(c.join("")):
+               ((c['split'])?(c.split("")):c);
+    })
+    ("p")(function(c,rr){
+        console.log("p#",c);
+        return c;
+    })
+    ("alphabet")(function(){
+        return "_".repeat(26).split('').map(function(c,i,a){
+            return String.fromCharCode(i+"a".charCodeAt(0));
+        });
+    })
+    ([1,2,3,4])(8)()
+    ("ss")("p")(1);
+
+    //toString().match(/(?:\/\*(?:[\s\S]*?)\*\/)/).pop().replace(/^\/\*/, "").replace(/\*\/$/, "")
+
+
+
