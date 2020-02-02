46d07633a9807c501b12eee8589e3b11828f0e9a
岡 実 <okamino@gmail.com>
2020-02-02 14:36:48 +0900

    concat set mode

---
+++ func_025.js
@@ -0,0 +1,76 @@
+ (function(rr){
+    var l='';
+    var s='';
+    var a=[];
+    var o={};
+    var ret={};
+    var is = function(c){
+            var ss=Object.prototype.toString.call(c).slice(8, -1)
+//            console.log(typeof c,ss,(c)?true:false);
+            return function(sss){
+                var b = (sss==ss);
+                return function(f){
+                    (b==true)?f.apply():0;
+                    return function(ff){
+                        (b==false)?((ff)?ff.apply():0):0;
+                        return is(c);
+                    };
+                }
+            }
+        };
+    var set=function(c){
+        is(c)("String")(function(){
+            s=(s)?s:c;
+            (o[s])?(o[s].push(c)):(o[s]=[]);
+        })(function(){
+            (c!=null)?((o[s])?(o[s].push(c)):0):0;
+            a.push(c);
+        });
+        s=(c!=null)?s:'';
+        return (c!=null)?set:map;
+    };
+    var map=function(c){
+        is(c)
+        ("String")(function(){
+            s=c;
+        })()
+        ("Function")(function(){
+            is(o[s])("Array")(
+                function(){
+                   o[s] = o[s].map(c); 
+                }
+            )(function(){
+                var mat=(o[s])?o[s]:a;
+                ret=c.call(mat,mat)
+
+            });
+        })()
+        ("Array")(function(){
+            c.map(function(cc,ii,aa){
+                is(cc)("Function")(function(){
+                    cc.apply(o[s],[o[s]]);
+                })
+            })
+        })()
+
+       ;
+        return (c!=null)?map:set;
+    };
+    set(rr);
+    rr.set=set;            
+    rr.map=map;            
+    return set;
+})
+("types")
+    (1)(0)(NaN)//Number
+    ("2")(`test`)("")//String
+    (true)(false)//Boolean
+    (function(){})(Date)//functions
+    (/[a-z]/)([1,2,3])([])({})(new Date)()()
+("array")
+    (1)(1)(1)(1)(1)(1)(1)(1)(1)(1)()
+//("types")(console.log)
+("array")(function(c,i,a){return c*2})(function(c,i,a){return c*2})([console.log])
+//("array")(console.log)
+    ();
+
