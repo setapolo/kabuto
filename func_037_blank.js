c09336c419ee443560c8dea656585b8d860ba919
moka <oka@tij.ne.jp>
2020-02-14 18:06:04 +0900

    36,37

---
+++ func_037_blank.js
@@ -0,0 +1,81 @@
+  (function(rr){
+    var s='';
+    var a=[];
+    var o={};
+    var ret={};
+    var f=null;
+    var k=null;
+    var is = function(c,flg){
+            (flg)?(console.log(flg,c)):0;
+            return function(sss,flg){
+                var ss=Object.prototype.toString.call(c).slice(8, -1);
+                (flg)?(console.log(flg,ss)):0;
+                var type=typeof sss;
+                var b = (type=="boolean")?(c!=null):((type=="string")?(sss==ss):(typeof c==type));
+                b=(typeof c=="boolean")?c:b;
+                //console.log(c,"_is_",typeof c,ss,"_compareto_",sss,b);
+                return function(f){
+                    (b==true)?f.apply(c,[c,ss,sss]):0;
+                    return function(ff){
+                        (b==false)?((ff)?ff.apply(c,[c,ss,sss]):0):0;
+                        return is(c);
+                    };
+                }
+            }
+        };
+    var set=function(c){
+        //console.log("welcome set",c);
+        is(c)("String")(function(c){
+            a.push(c);
+        })
+        is(c)("Number")(function(c){
+            a.push(c);
+        })
+        return (c!=null)?set:map;
+    };
+    var map=function(c){
+        is(c,false)("Function")(function(c){
+            is(c.apply(rr,[rr]),true)
+        });
+        //console.log("welcome map",c);
+        return (c!=null)?map:set;
+    };
+    set(rr);
+    rr.set=set;            
+    rr.map=map;
+    rr.is=is;
+    rr.a=a;
+    return set;
+})({})("0")(4)()(function(rr){
+    //rr.is(rr,true);
+
+    var rep = (function(s){
+            return function(n){
+                rr.is(s.repeat(n),true);
+                return rep;
+            }});
+
+//    ["x",4,"z",8,"o",16].map(function(c,i,a){
+//        (i==0)?o=rep(c):((i%2)==0)?(o=b(c)):(b=o(c));
+//    });
+
+    var x=function(){
+        var a=[];
+        return function(c){
+            a.push(c);
+            return (c)?x:a;
+        }
+    }()
+    x("x")(2)
+     ("z")(3)
+     ("o")(8)().map(function(c,i,a){
+        (i==0)?o=rep(c):((i%2)==0)?(o=b(c)):(b=o(c));
+    });
+
+
+//    return (function(s,n){return s.repeat(n)}).apply(rr,rr.a);
+});
+
+
+;
+
