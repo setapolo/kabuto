ac3d0768ad39e8bfb2189b20b5a9343f7ad683c6
岡 実 <okamino@gmail.com>
2020-03-09 08:44:57 +0900

    concat set mode

--- func_068.js
+++ func_068.js
@@ -3,8 +3,7 @@
   var ret=[];
   var rule=function(head){
     ret.push(head);
-    var ff=function(k){
-      var fff=function(v){
+    var ff=function(k){var fff=function(v){
         ret.push([k,v]);
         return ff
       };
@@ -12,17 +11,19 @@
     }
     return ff;
   }
-  var f=function(){
-    var aa =[].slice.call(arguments); 
-    console.log((aa[0]!=null)?(0):(ret.reduce(
-        function(p,c,i,a){
-            return [p,c].join(" ");
-            //p =(i==1)?p+"{":p;
-            //return [p ,c.reduce(function(pp,cc,ii,aa){return [pp,": ",cc,"; "].join("")})].join(" ");
-        }
-        )+"}"
-    ));
-    return (aa[0]!=null)?rule:rule;
+  var f=function(top){
+    console.log((top!=null)?(0):function(){
+           tt=ret.shift();
+          return ret.reduce(function(p,c,i,a){
+            var s = (i==1)?(function(){
+                a.shift();
+                return [tt," ","{",p.join(":"),"; ",c.join(":"),";"].join("");
+            })():p+c+" <"+(typeof c) + "> "+c;
+            a.shift();
+            return s;
+          })+"}";  
+    }()) 
+    return (top!=null)?rule:rule;
   }
   return f;
 })(global)
@@ -30,16 +31,5 @@
         ("body")
           ("background")("black")
           ("color")("gray")()
-        ("body2")
-          ("background")("black")
-          ("color")("gray")()
-        ("body3")
-          ("background")("black")
-          ("color")("gray")()
-        ("body4")
-          ("background")("black")
-          ("color")("gray")()
-
-
-      ;
+          ;
 
